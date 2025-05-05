9f70e848d1e763a05877aa5159f8e9420ca47946
Nicolas Schmitt <nicoleonschmitt@gmail.com>
2025-05-05 11:47:34 +0200

    Add a test for the TinyBlocks-MonoInc case

--- test/schemes/TinyBlocks.cpp
+++ test/schemes/TinyBlocks.cpp
@@ -255,3 +255,30 @@ TEST(TinyBlocksTest, RLEDecompressionInvariant64bit) {
     ASSERT_EQ(column.data()[i], decompression_out[i]);
   }
 }
+//---------------------------------------------------------------------------
+// Verifies that the data remains unchanged after MonoInc compression and
+// decompression for 32 bit integers.
+TEST(TinyBlocksTest, MonoIncDecompressionInvariant) {
+  constexpr uint16_t kBlockSize = 256;
+
+  auto path = "../data/tpch/sf1/customer.tbl";
+  auto column = storage::Column<INTEGER>::fromFile(path, 0, '|');
+
+  column.padToMultipleOf(kBlockSize);
+
+  ColumnCompressor<INTEGER, kBlockSize> compressor(
+      column, CompressionSchemeType::kTinyBlocks);
+
+  // compress
+  std::unique_ptr<compression::u8[]> compression_out;
+  compressor.compress(compression_out);
+
+  // decompress
+  std::vector<compression::INTEGER> decompression_out;
+  compressor.decompress(decompression_out, compression_out.get());
+
+  // verify
+  for (size_t i = 0; i < column.size(); ++i) {
+    ASSERT_EQ(column.data()[i], decompression_out[i]);
+  }
+}
