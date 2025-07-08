c2612015e4fd2790a9cbf6665d7a0719aac7f380
Nicolas Schmitt <nicoleonschmitt@gmail.com>
2025-07-08 10:50:22 +0200

    Add test cases that include negative & positive values

--- test/tinyblocks/TinyBlocks.cpp
+++ test/tinyblocks/TinyBlocks.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <random>
 //---------------------------------------------------------------------------
 #include "storage/Column.hpp"
 #include "tinyblocks/DataBlock.hpp"
@@ -6,14 +7,38 @@
 using namespace compression;
 using namespace compression::tinyblocks;
 //---------------------------------------------------------------------------
+enum class ColumnName { kPartSupp, kNegativePositive, kMonotonic };
+/// A wrapper for the ps_partkey column in TPCH.
 template <typename T> struct Partsupp {
   static constexpr const char *path = "../data/tpch/sf1/partsupp.tbl";
   static constexpr u32 index = 0;
+  static constexpr ColumnName name = ColumnName::kPartSupp;
   static vector<T> make_data() { return {}; }
 };
+/// A wrapper for a column including negative and positive values.
+template <typename T> struct NegativePositive {
+  static constexpr const char *path = nullptr;
+  static constexpr u32 index = 0;
+  static constexpr ColumnName name = ColumnName::kNegativePositive;
+  static vector<T> make_data() {
+    const u32 kSize = datablock::kDefaultSize * 16;
+    vector<T> data(kSize);
+    // Generate random values
+    std::mt19937 rng(42);
+    std::uniform_int_distribution<T> dist(
+        std::numeric_limits<int8_t>::lowest() / 2,
+        std::numeric_limits<int8_t>::max() / 2);
+    for (u32 i = 0; i < kSize; ++i) {
+      data[i] = dist(rng);
+    }
+    return data;
+  }
+};
+/// A wrapper for a monotonically increasing column.
 template <typename T> struct Monotonic {
   static constexpr const char *path = nullptr;
   static constexpr u32 index = 0;
+  static constexpr ColumnName name = ColumnName::kMonotonic;
   static vector<T> make_data() {
     const u32 kSize = datablock::kDefaultSize * 16;
     vector<T> data(kSize);
@@ -44,7 +69,47 @@ struct TinyBlocksConfig {
 //---------------------------------------------------------------------------
 /// Test-Suites for TinyBlocks.
 template <typename Config>
-class TinyBlocksTestCompression : public ::testing::Test {};
+class TinyBlocksTestCompression : public ::testing::Test {
+protected:
+  void TestDecompressionInvariant() {
+    constexpr u16 kTinyBlockSize = Config::kTinyBlockSize;
+    //---------------------------------------------------------------------------
+    using DataType = typename Config::DataType;
+    using DataBlocks = datablock::DataBlock<DataType, kTinyBlockSize>;
+    //---------------------------------------------------------------------------
+    storage::Column<DataType> column(vector<DataType>{});
+    if constexpr (Config::ColumnMeta::name == ColumnName::kPartSupp) {
+      // NOT Monotonically Increasing
+      column = storage::Column<DataType>::fromFile(Config::ColumnMeta::path,
+                                                   Config::ColumnMeta::index,
+                                                   kColumnSeperator);
+      column.padToMultipleOf(kTinyBlockSize);
+    } else {
+      column = storage::Column<DataType>(Config::ColumnMeta::make_data());
+    }
+    //---------------------------------------------------------------------------
+    // compress
+    auto compression_out =
+        std::make_unique<u8[]>(column.size() * sizeof(DataType) * 2);
+    DataBlocks tb;
+    tb.compress(column.data(), column.size(), compression_out.get(),
+                &Config::kScheme);
+    //---------------------------------------------------------------------------
+    // decompress
+    vector<INTEGER> decompression_out(column.size());
+    tb.decompress(decompression_out.data(), column.size(),
+                  compression_out.get());
+    //---------------------------------------------------------------------------
+    // verify
+    for (size_t i = 0; i < column.size(); ++i) {
+      EXPECT_EQ(column.data()[i], decompression_out[i]);
+    }
+  }
+};
+template <typename Config>
+class TinyBlocksTestCompression1 : public TinyBlocksTestCompression<Config> {};
+template <typename Config>
+class TinyBlocksTestCompression2 : public TinyBlocksTestCompression<Config> {};
 template <typename Config>
 class TinyBlocksTestFiltering : public ::testing::Test {};
 //---------------------------------------------------------------------------
@@ -135,6 +200,62 @@ using Config38 =
     TinyBlocksConfig<INTEGER, Partsupp<INTEGER>, 64, Scheme::PFOR_DELTA>;
 using Config39 =
     TinyBlocksConfig<INTEGER, Partsupp<INTEGER>, 64, Scheme::PFOR_LEMIRE>;
+using Config1220 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 128, Scheme::FOR>;
+using Config222 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 128, Scheme::RLE4>;
+using Config322 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 128, Scheme::RLE8>;
+using Config522 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 128, Scheme::PFOR>;
+using Config622 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 128, Scheme::PFOR_EBP>;
+using Config722 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 128, Scheme::PFOR_EP>;
+using Config922 = TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 128,
+                                   Scheme::PFOR_LEMIRE>;
+using Config1122 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 256, Scheme::FOR>;
+using Config1222 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 256, Scheme::RLE4>;
+using Config1322 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 256, Scheme::RLE8>;
+using Config1522 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 256, Scheme::PFOR>;
+using Config1622 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 256, Scheme::PFOR_EBP>;
+using Config1722 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 256, Scheme::PFOR_EP>;
+using Config1922 = TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 256,
+                                    Scheme::PFOR_LEMIRE>;
+using Config2122 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 512, Scheme::FOR>;
+using Config2222 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 512, Scheme::RLE4>;
+using Config2322 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 512, Scheme::RLE8>;
+using Config2522 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 512, Scheme::PFOR>;
+using Config2622 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 512, Scheme::PFOR_EBP>;
+using Config2722 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 512, Scheme::PFOR_EP>;
+using Config2922 = TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 512,
+                                    Scheme::PFOR_LEMIRE>;
+using Config3122 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 64, Scheme::FOR>;
+using Config3222 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 64, Scheme::RLE4>;
+using Config3322 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 64, Scheme::RLE8>;
+using Config3522 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 64, Scheme::PFOR>;
+using Config3622 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 64, Scheme::PFOR_EBP>;
+using Config3722 =
+    TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 64, Scheme::PFOR_EP>;
+using Config3922 = TinyBlocksConfig<INTEGER, NegativePositive<INTEGER>, 64,
+                                    Scheme::PFOR_LEMIRE>;
 using Config40 =
     TinyBlocksConfig<INTEGER, Monotonic<INTEGER>, 64, Scheme::MONOTONIC>;
 using Config41 =
@@ -143,6 +264,8 @@ using Config42 =
     TinyBlocksConfig<INTEGER, Monotonic<INTEGER>, 256, Scheme::MONOTONIC>;
 using Config43 =
     TinyBlocksConfig<INTEGER, Monotonic<INTEGER>, 512, Scheme::MONOTONIC>;
+//---------------------------------------------------------------------------
+/// The test cases declarations.
 using Configs =
     ::testing::Types<Config1, Config2, Config3, Config4, Config5, Config6,
                      Config7, Config8, Config9, Config11, Config12, Config13,
@@ -151,49 +274,29 @@ using Configs =
                      Config27, Config28, Config29, Config31, Config32, Config33,
                      Config34, Config35, Config36, Config37, Config38, Config39,
                      Config40, Config41, Config42, Config43>;
+using ConfigsNegPos =
+    ::testing::Types<Config1220, Config222, Config322, Config522, Config622,
+                     Config722, Config922, Config1122, Config1222, Config1322,
+                     Config1522, Config1622, Config1722, Config1922, Config2122,
+                     Config2222, Config2322, Config2522, Config2622, Config2722,
+                     Config2922, Config3122, Config3222, Config3322, Config3522,
+                     Config3622, Config3722, Config3922>;
 using FilterConfigs =
     ::testing::Types<Config1, Config100, Config101, Config102, Config11,
                      Config110, Config111, Config112, Config21, Config120,
                      Config121, Config122, Config31, Config130, Config131,
                      Config132, Config40, Config41, Config42, Config43>;
-TYPED_TEST_CASE(TinyBlocksTestCompression, Configs);
+TYPED_TEST_CASE(TinyBlocksTestCompression1, Configs);
+TYPED_TEST_CASE(TinyBlocksTestCompression2, ConfigsNegPos);
 TYPED_TEST_CASE(TinyBlocksTestFiltering, FilterConfigs);
 //---------------------------------------------------------------------------
 /// A Test for TinyBlocks confirming compression and decompression does not
 /// alter the data.
-TYPED_TEST(TinyBlocksTestCompression, CompDecompInvariant) {
-  constexpr u16 kTinyBlockSize = TypeParam::kTinyBlockSize;
-  //---------------------------------------------------------------------------
-  using DataType = typename TypeParam::DataType;
-  using DataBlocks = datablock::DataBlock<DataType, kTinyBlockSize>;
-  //---------------------------------------------------------------------------
-  storage::Column<DataType> column(vector<DataType>{});
-  if constexpr (TypeParam::ColumnMeta::path) {
-    // NOT Monotonically Increasing
-    column = storage::Column<DataType>::fromFile(TypeParam::ColumnMeta::path,
-                                                 TypeParam::ColumnMeta::index,
-                                                 kColumnSeperator);
-    column.padToMultipleOf(kTinyBlockSize);
-  } else {
-    // Monotonically Increasing
-    column = storage::Column<DataType>(TypeParam::ColumnMeta::make_data());
-  }
-  //---------------------------------------------------------------------------
-  // compress
-  auto compression_out =
-      std::make_unique<u8[]>(column.size() * sizeof(DataType) * 2);
-  DataBlocks tb;
-  tb.compress(column.data(), column.size(), compression_out.get(),
-              &TypeParam::kScheme);
-  //---------------------------------------------------------------------------
-  // decompress
-  vector<INTEGER> decompression_out(column.size());
-  tb.decompress(decompression_out.data(), column.size(), compression_out.get());
-  //---------------------------------------------------------------------------
-  // verify
-  for (size_t i = 0; i < column.size(); ++i) {
-    EXPECT_EQ(column.data()[i], decompression_out[i]);
-  }
+TYPED_TEST(TinyBlocksTestCompression1, CompDecompInvariant) {
+  this->TestDecompressionInvariant();
+}
+TYPED_TEST(TinyBlocksTestCompression2, CompDecompInvariant) {
+  this->TestDecompressionInvariant();
 }
 //---------------------------------------------------------------------------
 /// Testing TinyBlocks filtering capabilities.
@@ -207,7 +310,7 @@ TYPED_TEST(TinyBlocksTestFiltering, Filtering) {
   algebra::Predicate<INTEGER> pred(TypeParam::kPredicate, 42);
   //---------------------------------------------------------------------------
   vector<INTEGER> vec(kSize);
-  if constexpr (TypeParam::ColumnMeta::path) {
+  if constexpr (TypeParam::ColumnMeta::name == ColumnName::kPartSupp) {
     // NOT Monotonically Increasing
     for (u32 i = 0; i < vec.size(); ++i) {
       if (i % 42 == 0)
