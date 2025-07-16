172e5817b8e848fd644c11809e3dd79b53607a97
Nicolas Schmitt <nicoleonschmitt@gmail.com>
2025-07-16 11:55:37 +0200

    Add more test cases for Block512 bitpacking
    In particular, add test cases testing whether compressing and subsequent decompressing do not alter the data

--- test/bitpacking/simd32bit/Block512BitPacking.cpp
+++ test/bitpacking/simd32bit/Block512BitPacking.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <memory>
+#include <random>
 //---------------------------------------------------------------------------
 #include "bitpacking/simd32bit/Block512BitPacking.hpp"
 //---------------------------------------------------------------------------
@@ -237,5 +238,112 @@ TEST(AVX512BitPackingTest, LTFilterMultiple) {
       ASSERT_EQ(matches[i], 0);
     else
       ASSERT_GT(matches[i], 0);
+  }
+}
+//---------------------------------------------------------------------------
+/// Compile-Time configuration parameters.
+template <typename T, u8 kBit> struct Config {
+  using DataType = T;
+  //---------------------------------------------------------------------------
+  static constexpr u8 kPackSize = kBit;
+};
+//---------------------------------------------------------------------------
+template <typename Config>
+class BP512CompDecompInvariant : public ::testing::Test {};
+template <typename Config>
+class BP512FastCompDecompInvariant : public ::testing::Test {};
+//---------------------------------------------------------------------------
+using C0 = Config<INTEGER, 0>;
+using C1 = Config<INTEGER, 1>;
+using C2 = Config<INTEGER, 2>;
+using C3 = Config<INTEGER, 3>;
+using C4 = Config<INTEGER, 4>;
+using C5 = Config<INTEGER, 5>;
+using C6 = Config<INTEGER, 6>;
+using C7 = Config<INTEGER, 7>;
+using C8 = Config<INTEGER, 8>;
+using C9 = Config<INTEGER, 9>;
+using C10 = Config<INTEGER, 10>;
+using C11 = Config<INTEGER, 11>;
+using C12 = Config<INTEGER, 12>;
+using C13 = Config<INTEGER, 13>;
+using C14 = Config<INTEGER, 14>;
+using C15 = Config<INTEGER, 15>;
+using C16 = Config<INTEGER, 16>;
+using C17 = Config<INTEGER, 17>;
+using C18 = Config<INTEGER, 18>;
+using C19 = Config<INTEGER, 19>;
+using C20 = Config<INTEGER, 20>;
+using C21 = Config<INTEGER, 21>;
+using C22 = Config<INTEGER, 22>;
+using C23 = Config<INTEGER, 23>;
+using C24 = Config<INTEGER, 24>;
+using C25 = Config<INTEGER, 25>;
+using C26 = Config<INTEGER, 26>;
+using C27 = Config<INTEGER, 27>;
+using C28 = Config<INTEGER, 28>;
+using C29 = Config<INTEGER, 29>;
+using C30 = Config<INTEGER, 30>;
+using C31 = Config<INTEGER, 31>;
+using C32 = Config<INTEGER, 32>;
+using Configs =
+    ::testing::Types<C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13,
+                     C14, C15, C16, C17, C18, C19, C20, C21, C22, C23, C24, C25,
+                     C26, C27, C28, C29, C30, C31, C32>;
+using FastPackConfigs = ::testing::Types<C1, C2>;
+//---------------------------------------------------------------------------
+TYPED_TEST_CASE(BP512CompDecompInvariant, Configs);
+TYPED_TEST(BP512CompDecompInvariant, CompDecompInvariant) {
+  const u32 kSize = 512;
+  //---------------------------------------------------------------------------
+  // Generate random data.
+  vector<INTEGER> vec(kSize);
+  std::mt19937 rng(42);
+  std::uniform_int_distribution<typename TypeParam::DataType> dist(0, 1);
+  for (u32 i = 0; i < kSize; ++i) {
+    vec[i] = dist(rng);
+  }
+  //---------------------------------------------------------------------------
+  // Compression.
+  auto compressed = std::make_unique<__m512i[]>(64);
+  bitpacking::simd32::avx512::pack(reinterpret_cast<u32 *>(vec.data()),
+                                   compressed.get(), TypeParam::kPackSize);
+  //---------------------------------------------------------------------------
+  // Decompression.
+  vector<INTEGER> out(512);
+  bitpacking::simd32::avx512::unpack(compressed.get(),
+                                     reinterpret_cast<u32 *>(out.data()),
+                                     TypeParam::kPackSize);
+  //---------------------------------------------------------------------------
+  for (u32 i = 0; i < out.size(); ++i) {
+    ASSERT_EQ(out[i], vec[i]);
+  }
+}
+//---------------------------------------------------------------------------
+TYPED_TEST_CASE(BP512FastCompDecompInvariant, FastPackConfigs);
+TYPED_TEST(BP512FastCompDecompInvariant, FastCompDecompInvariant) {
+  const u32 kSize = 512;
+  //---------------------------------------------------------------------------
+  // Generate random data.
+  vector<INTEGER> vec(kSize);
+  std::mt19937 rng(42);
+  std::uniform_int_distribution<typename TypeParam::DataType> dist(0, 1);
+  for (u32 i = 0; i < kSize; ++i) {
+    vec[i] = dist(rng);
+  }
+  //---------------------------------------------------------------------------
+  // Compression.
+  auto compressed = std::make_unique<__m512i[]>(64);
+  bitpacking::simd32::avx512::packfast(reinterpret_cast<u32 *>(vec.data()),
+                                       compressed.get(), TypeParam::kPackSize);
+  //---------------------------------------------------------------------------
+  // Decompression.
+  vector<INTEGER> out(512);
+  bitpacking::simd32::avx512::unpackfast(compressed.get(),
+                                         reinterpret_cast<u32 *>(out.data()),
+                                         TypeParam::kPackSize);
+  //---------------------------------------------------------------------------
+  for (u32 i = 0; i < out.size(); ++i) {
+    ASSERT_EQ(out[i], vec[i]);
   }
 }
